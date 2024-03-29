5934569058c825c8fa92cd77783eee2e0ce9345d
Jackson Ozanne <s537079@nwmissouri.edu>
2024-03-29 11:48:51 -0500

    GraduateStudent Class methods

--- src/studentdriver/GraduateStudent.java
+++ src/studentdriver/GraduateStudent.java
@@ -1,5 +1,42 @@
 package studentdriver;
 
-public class GraduateStudent {
+public class GraduateStudent extends StudentFees {
 
+    private int coursesEnrolled;
+    private boolean isGraduateAssistant;
+    private String graduateAssistantType;
+    double ADDITIONAL_FEES = 654.45;
+
+    public GraduateStudent(String studentName, int studentID, boolean isEnrolled, boolean isGraduateAssistant, String graduateAssistantType, int coursesEnrolled) {
+        this.studentName = studentName;
+        this.studentID = studentID;
+        this.isEnrolled = isEnrolled;
+        this.isGraduateAssistant = isGraduateAssistant;
+        this.graduateAssistantType = graduateAssistantType;
+        this.coursesEnrolled = coursesEnrolled;
+    }
+
+    public GraduateStudent(String studentName, int studentID, boolean isEnrolled, boolean isGraduateAssistant, int coursesEnrolled) {
+        this.studentName = studentName;
+        this.studentID = studentID;
+        this.isEnrolled = isEnrolled;
+        this.isGraduateAssistant = isGraduateAssistant;
+        this.coursesEnrolled = coursesEnrolled;
+    }
+
+    public boolean isIsGraduateAssistant() {
+        return isGraduateAssistant;
+    }
+
+    public int getCoursesEnrolled() {
+        return coursesEnrolled;
+    }
+
+    //Must add assitanceship calculation
+    //public double getPayableAmount(){
+    //    return (((coursesEnrolled * CREDITS_PER_COURSE) * PER_CREDIT_FEE) + ADDITIONAL_FEES);
+    //}
+    //@Override
+    //public String toString(){
+    //    return ;}
 }
